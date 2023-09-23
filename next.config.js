a8603dcac86669f9a4a3038dff16eee1d6a05c43
tetsuya_kojima <gangelion8@gmail.com>
2023-09-23 21:19:55 +0900

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/graphql', // GraphQLエンドポイントのパス
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // 許可するオリジンを指定
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'POST, OPTIONS', // 許可するHTTPメソッドを指定
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Accept', // 許可するヘッダーを指定
+          },
+        ],
+      },
+    ];
+  },
+}
 
 module.exports = nextConfig
