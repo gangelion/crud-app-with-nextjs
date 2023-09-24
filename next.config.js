d6b1637ee67ba79341710322abe78e30f50da6f3
tetsuya_kojima <gangelion8@gmail.com>
2023-09-24 10:30:09 +0900

    revert unnecessary cors settings

--- next.config.js
+++ next.config.js
@@ -1,26 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/api/graphql', // GraphQLエンドポイントのパス
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*', // 許可するオリジンを指定
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'POST, OPTIONS', // 許可するHTTPメソッドを指定
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-Requested-With, Content-Type, Accept', // 許可するヘッダーを指定
-          },
-        ],
-      },
-    ];
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,7 +1,6 @@
 import { ApolloServer } from "apollo-server-micro";
 import { typeDefs } from "@/apollo/type-defs";
 import { resolvers } from "@/apollo/resolvers";
-import Cors from 'micro-cors';
 
 export const config = {
   api: {
@@ -9,16 +8,6 @@ export const config = {
   },
 };
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers })
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
-const cors = Cors()
-export default cors(async function handler(req, res) {
-  if (req.method === 'OPTIONS') {
-    res.end()
-    return false
-  }
-
-  await apolloServer.createHandler({
-    path: '/api/graphql',
-  })(req, res)
-})
+export default apolloServer.createHandler({ path: "/api/graphql" });
